a1fe557827050e90568a78ece57823b018ef6f2c
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-07-05 22:35:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,7 +10,7 @@ def game_hash
         :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebounds => 1,
+        :rebounds => 12,
         :assists => 12,
         :steals => 3,
         :blocks => 1,
